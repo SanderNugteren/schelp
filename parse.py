c8c9256d763a3e55c5fdc9a08f4b69875684eb3e
Chiel Kooijman <chiel999@gmail.com>
2012-10-30 18:05:22 +0100

    Can now generate frequency table and get probability of rule

--- parse.py
+++ parse.py
@@ -1,10 +1,44 @@
 #!/usr/bin/env python2
-from pyparsing import *
+from pyparsing import Forward, nestedExpr, Word, alphas
 
 sample = "(TOP (S (NP (DT A) (NP@ (NN record) (NN date))) (S@ (VP (VBZ has) (VP@ (RB n't) (VP (VBN been) (VP (VBN set))))) (. .))) )"
 
+characters = alphas + '@' + "'" + "," + "."
+
 enclosed = Forward()
 nestedParens = nestedExpr('(', ')', content=enclosed)
-enclosed << (Word(alphas+"@"+"'"+".") | nestedParens)
+enclosed << (Word(characters) | nestedParens)
 
-print enclosed.parseString(sample).asList()
+l = enclosed.parseString(sample).asList()
+
+
+def get_p(t, f):
+    return f[t[0]][t[1:]] / float(sum(f[t[0]].values()))
+
+
+def traverse(o):
+    if isinstance(o, list):
+        if isinstance(o[1], str):
+            yield (o[0], o[1])
+        else:
+            if len(o) > 2:
+                yield (o[0], o[1][0], o[2][0])
+            for value in o:
+                for subvalue in traverse(value):
+                    yield subvalue
+
+
+frequencies = dict()
+for t in traverse(l[0][1]): # skip TOP
+    if t[0] in frequencies:
+        if t[1:] in frequencies[t[0]]:
+            frequencies[t[0]][t[1:]] += 1
+        else:
+            frequencies[t[0]][t[1:]] = 1
+    else:
+        frequencies[t[0]] = {t[1:]: 1}
+
+
+#print frequencies
+
+#print get_p(('S', 'NP', 'S@'), frequencies)
