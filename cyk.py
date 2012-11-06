89899928f9964776d5b3106f47d82b245082cd9b
Sander Nugteren <sander.nugteren@gmail.com>
2012-11-06 17:22:58 +0100

    cyk working, tested for first sentence.

--- cyk.py
+++ cyk.py
@@ -1,4 +1,5 @@
 import sys
+from itertools import combinations
 from freqs import frequencies
 from reverse import reverse
 
@@ -6,20 +7,27 @@ def main():
     f = open('data/test_sentence')
     for line in f:
         words = line.strip().split(" ")
-        table = [[[]] * i for i in xrange(1, len(words) + 1)]
-        for i,j in table_traverse(len(words)):
+        table = [[set()] * i for i in xrange(1, len(words) + 1)]
+        for i, j in table_traverse(len(words)):
             if i == j:
-                table[i][j] = reverse[word[i]]
-                new_keys = []
+                table[i][j] = set(reverse[(words[i],)])
+                new_keys = set()
                 keys_added = True
                 while keys_added:
                     keys_added = False
                     for rule in table[i][j]:
-                        for key in rule.iterkeys():
-                            new_keys.append(key)
-                            keys_added = True
+                        if rule in reverse:
+                            for key in reverse[rule]:
+                                new_keys |= key
+                                keys_added = True
             else:
-                pass
+                left = set(table[j-1][i])
+                down = set(table[j][i+1])
+                for l in left:
+                    for d in down:
+                        if (l, d) in reverse:
+                            table[j][i] |= set(reverse[(l, d)])
+        print table
         sys.exit(0)
 
 def table_traverse(table_length):
