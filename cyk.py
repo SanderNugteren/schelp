85071c3061a5ca3f75f31a007fa190438866e011
Chiel Kooijman <chiel999@gmail.com>
2012-12-23 15:44:42 +0100

    working on p in unary

--- cyk.py
+++ cyk.py
@@ -7,7 +7,7 @@ from pprint import pprint
 def main(filename):
     f = open(filename)
     for table in cyk(f):
-        pprint(table)
+        #pprint(table)
         sys.exit(0)
         print(viterbi(table))
 
@@ -26,10 +26,18 @@ def cyk(f):
         table = [[set()] * i for i in xrange(1, len(words) + 1)] # CYK table
         for i, j in table_traverse(len(words)):
             if i == j:  # Unary rules
-                table[i][j] = set(r_rules[(words[i],)]) # Add terminal rules
+                terminals = r_rules[(words[i],)]
+                if len(terminals) == 1:
+                    table[i][j] = (terminals[0], 0) # FIXME log of p(1)
+                else:
+                    total = 0
+                    for t in terminals:
+                        get_p((word[i], t) # fix probs
+                print words[i], table[i][j]
+                print
                 new_keys = set()
                 keys_added = True
-                while keys_added: # Add nonterminal unary rules, FIXME broken
+                while keys_added: # Add nonterminal unary rules
                     keys_added = False
                     for rule in table[i][j]:
                         if (rule,) in r_rules:
@@ -48,7 +56,7 @@ def cyk(f):
                             #print 'ld: ' + str((l, d))
                             if (l, d) in r_rules:
                                 for parent in r_rules[(l, d)]:
-                                    print parent + '->' + str(l) + ' ' + str(d)
+                                    #print parent + '->' + str(l) + ' ' + str(d)
                                     p = get_logp((parent, l, d), frequencies)
                                     table[j][i].add((parent, l, d, p))
         yield table
