817893beea9c43a5cfa1d8e4f2eddad0579de8dd
Chiel Kooijman <chiel999@gmail.com>
2012-12-30 21:28:00 +0100

    Fixed tree representation double tuple bug

--- cyk.py
+++ cyk.py
@@ -10,7 +10,8 @@ def main(filename):
     f = open(filename)
     for table in cyk(f):
         #pprint(table)
-        print table[-1][0]
+        print ('S', table[-1][0]['S'][1])
+        #pprint(table[1][0]['NP'])
 
 
 def cyk(f):
@@ -21,13 +22,14 @@ def cyk(f):
             if i == j:  # Unary rules
                 terminals = r_rules[(words[i],)]
                 if len(terminals) == 1: # log p(rule|terminal) = 0
-                    table[i][j][terminals[0]] = 0
+                    table[i][j][terminals[0]] = (0.0, words[0])
                 else: # log p(rule|terminal)
                     total = 0.0
                     for t in terminals:
                         total += frequencies[t][(words[i],)]
                     for t in terminals:
-                        table[i][j][t] = log(frequencies[t][(words[i],)]/total)
+                        table[i][j][t] = (log(frequencies[t][(words[i],)]/total), words[0])
+                new_rules = set()
                 new_keys = set()
                 keys_added = True
                 while keys_added: # Add nonterminal unary rules
@@ -35,11 +37,13 @@ def cyk(f):
                     for rule in table[i][j]:
                         if (rule,) in r_rules:
                             for key in r_rules[(rule,)]:
+                                #print key + '->' + rule
                                 if key not in new_keys:
-                                    new_keys |= set([key])
+                                    new_keys.add(key)
+                                    new_rules.add((key, rule))
                                     keys_added = True
-                for key in new_keys:
-                    table[i][j][key] = 0
+                for key, rule in new_rules:
+                    table[i][j][key] = (-1, (rule, words[i])) # FIXME real prob
             else:   # Binary rules
                 for ll in range(i, j):
                     #dd = ll + 1
@@ -49,18 +53,23 @@ def cyk(f):
                     for l in left.keys():
                         for d in down.keys():
                             if (l, d) in r_rules:
-                                pl = left[l]
-                                pd = down[d]
+                                pl = left[l][0]
+                                pd = down[d][0]
                                 for parent in r_rules[(l, d)]:
                                     #print parent + '->' + str(l) + ' ' + str(d)
                                     rule = (parent, l, d)
                                     p = get_logp(rule , frequencies) + pl + pd
                                     #check if there already is an entry for this rule
                                     if parent in table[j][i]:
-                                        if table[j][i][parent] < p:
-                                            table[j][i][parent] = p
+                                        if table[j][i][parent][0] < p:
+                                            print l + '->' + str(left[l][1:])
+                                            print d + '->' + str(down[d][1:])
+                                            print
+                                            print
+                                            table[j][i][parent] = (p, ((l, left[l][-1]), (d, down[d][-1])))
                                     else:
-                                        table[j][i][parent] = p
+                                        table[j][i][parent] = (p, ((l, left[l][-1]), (d, down[d][-1])))
+                                    #pprint(table[j][i][parent])
         yield table
 
 
