e6446d95a35017d575da066b0b9b13d10969e81a
Chiel Kooijman <chiel999@gmail.com>
2013-01-03 17:02:29 +0100

    parent node in name (freqs2)

--- parse.py
+++ parse.py
@@ -30,13 +30,30 @@ def traverse(o):
     except IndexError:
         yield ()
 
+def traverse_parent(parent, o):
+    """Append parent tag to child for better context awareness.
+    """
+    try:
+        if isinstance(o[1], basestring):
+            yield (parent + '/' + o[0], o[1])  # Terminal
+        else:
+            if len(o) > 2:      # Non-terminal
+                yield (parent + '/' + o[0], o[0] + '/' + o[1][0], o[0] + '/' + o[2][0])
+            else:
+                yield (parent + '/' + o[0], o[0] + '/' + o[1][0])
+            for value in o[1:]:
+                for subvalue in traverse_parent(o[0], value):
+                    yield subvalue
+    except IndexError:
+        yield ()
+
 
 def main():
     f = open('data/train_trees')
     frequencies = dict()
     for i, line in enumerate(f):
         l = tree_parser.get_tree(line)  # charity from Patrick de Kok
-        for t in traverse(l[1]): # skip TOP
+        for t in traverse_parent('TOP', l[1]): # skip TOP
             if t[0] in frequencies:
                 if t[1:] in frequencies[t[0]]:
                     frequencies[t[0]][t[1:]] += 1
--- parse_reverse.py
+++ parse_reverse.py
@@ -2,28 +2,7 @@
 from pyparsing import Forward, nestedExpr, Word, alphanums
 import tree_parser
 import sys
-
-
-def get_p(t, f):
-    if t[0] in f:
-        return f[t[0]][t[1:]] / float(sum(f[t[0]].values()))
-    return 0.0
-
-def get_logp(t, f):
-    return log(get_p(t, f))
-
-
-def traverse(o):
-    if isinstance(o[1], basestring):
-        yield (o[0], o[1])  # Terminal
-    else:
-        if len(o) > 2:      # Non-terminal
-            yield (o[0], o[1][0], o[2][0])
-        else:
-            yield (o[0], o[1][0])
-        for value in o[1:]:
-            for subvalue in traverse(value):
-                yield subvalue
+from parse import traverse, traverse_parent
 
 
 def main():
@@ -47,7 +26,7 @@ def main():
     frequencies = dict()
     for line in f:
         l = tree_parser.get_tree(line)  # charity from Patrick de Kok
-        for t in traverse(l[1]): # skip TOP
+        for t in traverse_parent('TOP', l[1]): # skip TOP
             if t[1:] in frequencies:
                 if not t[0] in frequencies[t[1:]]:
                     frequencies[t[1:]].append(t[0])
